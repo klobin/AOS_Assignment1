b664ef8c91bd99776e047893c4e2f4c6b8fb6c06
pthakare <pranayjeet.thakare@hitachivantara.com>
2019-10-15 22:38:53 -0700

    SJF

--- AOS Project/src/SJF.java
+++ AOS Project/src/SJF.java
@@ -1,11 +1,11 @@
 import java.util.*;
 
 public class SJF {
-    private int nextEligibleProcess(List<Process> processes) {
+    private int nextEligibleProcess(List<Process> processes, HashSet<Integer> visited) {
         int min = Integer.MAX_VALUE;
-        int res =-1;
+        int res = -1;
         for (int i = 0; i < processes.size(); i++) {
-            if(processes.get(i).getRunTime() < min){
+            if (processes.get(i).getRunTime() < min && !visited.contains(i)) {
                 res = i;
                 min = processes.get(i).getRunTime();
             }
@@ -17,8 +17,39 @@ public class SJF {
         int totalTurnAroundTime = 0;
         int totalWaitingTime = 0;
         int totalResponseTime = 0;
+        StringBuilder processesRunning = new StringBuilder();
+        HashSet<Integer> visited = new HashSet<>();
 
-//        while()
+        System.out.println("\n\nRunning Shortest Job First");
+        System.out.println("Name" + "\t" + "Arrival Time" + "\t" + "Run Time" + "\t" + "Priority" + "\t" + "Turn Around Time" + "\t" + "Waiting Time" + "\t" + "Response Time");
+
+        int nextIdx = nextEligibleProcess(processes, visited);
+        int clock = processes.get(nextIdx).getArrivalTime();
+
+        while (nextIdx >= 0) {
+            clock = clock + processes.get(nextIdx).getRunTime();
+            processes.get(nextIdx).setCompletionTime(clock);
+            processes.get(nextIdx).setTurnAroundTime(processes.get(nextIdx).getCompletionTime() - processes.get(nextIdx).getArrivalTime());
+            int wt = processes.get(nextIdx).getTurnAroundTime() - processes.get(nextIdx).getRunTime();
+            if(wt<0){
+                wt=0;
+            }
+            processes.get(nextIdx).setWaitingTime(wt);
+            processes.get(nextIdx).setResponseTime(processes.get(nextIdx).getTurnAroundTime() - processes.get(nextIdx).getRunTime());
+
+            processesRunning.append(processes.get(nextIdx).getName());
+            visited.add(nextIdx);
+            nextIdx = nextEligibleProcess(processes, visited);
+        }
+
+        for (Process p : processes) {
+            System.out.println(p.toString());
+        }
+
+        System.out.println("Processes Ran: " + processesRunning.toString());
+        System.out.println("\nAverage Turnaround Time: " + ((float) totalTurnAroundTime / (float) processes.size()));
+        System.out.println("Average Waiting Time: " + ((float) totalWaitingTime / (float) processes.size()));
+        System.out.println("Average Response Time: " + ((float) totalResponseTime / (float) processes.size()));
 
         return new Stat((float) totalTurnAroundTime / (float) processes.size(),
                 (float) totalResponseTime / (float) processes.size(),
