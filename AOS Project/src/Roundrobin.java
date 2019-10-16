eb240c62c501145fdeafa47c34f2acb0d9e170a8
Thomas Francis <thomas92francis@gmail.com>
2019-10-16 00:28:20 -0700

    Commit RoundRobin

--- AOS Project/src/Roundrobin.java
+++ AOS Project/src/Roundrobin.java
@@ -1,50 +1,51 @@
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-// Java program to implement Round Robin
-// Scheduling with different arrival time 
 public class Roundrobin {
     public Stat run(List<Process> processes) {
         int totalTurnAroundTime = 0;
         int totalWaitingTime = 0;
         int totalResponseTime = 0;
 
+        return roundRobin(processes, 2);
 
-        return new Stat((float) totalTurnAroundTime / (float) processes.size(),
-                (float) totalResponseTime / (float) processes.size(),
-                (float) totalWaitingTime / (float) processes.size());
+//        return new Stat((float) totalTurnAroundTime / (float) processes.size(),
+//                (float) totalResponseTime / (float) processes.size(),
+//                (float) totalWaitingTime / (float) processes.size());
     }
 
-    public static void roundRobin(String p[], int a[],
-                                  int b[], int n) {
+    public static Stat roundRobin(List<Process> processes, int n) {
         // result of average times
         int res = 0;
         int resc = 0;
+        Map<Character, Integer> startTimeMap = new HashMap<>();
 
         // for sequence storage
         String seq = new String();
 
         // copy the burst array and arrival array
         // for not effecting the actual array
-        int res_b[] = new int[b.length];
-        int res_a[] = new int[a.length];
+        int res_b[] = new int[processes.size()];
+        int res_a[] = new int[processes.size()];
 
         for (int i = 0; i < res_b.length; i++) {
-            res_b[i] = b[i];
-            res_a[i] = a[i];
+            res_b[i] = processes.get(i).getRunTime();
+            res_a[i] = processes.get(i).getArrivalTime();
         }
 
         // critical time of system
         int t = 0;
 
         // for store the waiting time
-        int w[] = new int[p.length];
+        int w[] = new int[processes.size()];
 
         // for store the Completion time
-        int comp[] = new int[p.length];
+        int comp[] = new int[processes.size()];
 
         while (true) {
             boolean flag = true;
-            for (int i = 0; i < p.length; i++) {
+            for (int i = 0; i < processes.size(); i++) {
 
                 // these condition for if
                 // arrival is not on zero
@@ -60,28 +61,31 @@ public class Roundrobin {
                                 t = t + n;
                                 res_b[i] = res_b[i] - n;
                                 res_a[i] = res_a[i] + n;
-                                seq += "->" + p[i];
+                                seq += "->" + processes.get(i).getName();
+                                startTimeMap.putIfAbsent(processes.get(i).getName(), t);
                             } else {
 
                                 // for last time
                                 t = t + res_b[i];
 
                                 // store comp time
-                                comp[i] = t - a[i];
+                                comp[i] = t - processes.get(i).getArrivalTime();
 
                                 // store wait time
-                                w[i] = t - b[i] - a[i];
+                                w[i] = t - processes.get(i).getRunTime() - processes.get(i).getArrivalTime();
                                 res_b[i] = 0;
 
                                 // add sequence
-                                seq += "->" + p[i];
+                                seq += "->" + processes.get(i).getName();
+                                startTimeMap.putIfAbsent(processes.get(i).getName(), t);
+
                             }
                         }
                     } else if (res_a[i] > n) {
 
                         // is any have less arrival time
                         // the coming process then execute them
-                        for (int j = 0; j < p.length; j++) {
+                        for (int j = 0; j < processes.size(); j++) {
 
                             // compare
                             if (res_a[j] < res_a[i]) {
@@ -91,13 +95,17 @@ public class Roundrobin {
                                         t = t + n;
                                         res_b[j] = res_b[j] - n;
                                         res_a[j] = res_a[j] + n;
-                                        seq += "->" + p[j];
+                                        seq += "->" + processes.get(j).getName();
+                                        startTimeMap.putIfAbsent(processes.get(i).getName(), t);
+
                                     } else {
                                         t = t + res_b[j];
-                                        comp[j] = t - a[j];
-                                        w[j] = t - b[j] - a[j];
+                                        comp[j] = t - processes.get(j).getArrivalTime();
+                                        w[j] = t - processes.get(j).getRunTime() - processes.get(j).getArrivalTime();
                                         res_b[j] = 0;
-                                        seq += "->" + p[j];
+                                        seq += "->" + processes.get(j).getName();
+                                        startTimeMap.putIfAbsent(processes.get(i).getName(), t);
+
                                     }
                                 }
                             }
@@ -113,19 +121,23 @@ public class Roundrobin {
                                 t = t + n;
                                 res_b[i] = res_b[i] - n;
                                 res_a[i] = res_a[i] + n;
-                                seq += "->" + p[i];
+                                seq += "->" + processes.get(i).getName();
+                                startTimeMap.putIfAbsent(processes.get(i).getName(), t);
+
                             } else {
                                 t = t + res_b[i];
-                                comp[i] = t - a[i];
-                                w[i] = t - b[i] - a[i];
+                                comp[i] = t - processes.get(i).getArrivalTime();
+                                w[i] = t - processes.get(i).getRunTime() - processes.get(i).getArrivalTime();
                                 res_b[i] = 0;
-                                seq += "->" + p[i];
+                                seq += "->" + processes.get(i).getName();
+                                startTimeMap.putIfAbsent(processes.get(i).getName(), t);
+
                             }
                         }
                     }
                 }
 
-                // if no process is come on thse critical
+                // if no process is come on these critical
                 else if (res_a[i] > t) {
                     t++;
                     i--;
@@ -137,37 +149,32 @@ public class Roundrobin {
             }
         }
 
+        int turnAroundTime = 0;
+        int responseTime = 0;
+        int waitingTime = 0;
+
         System.out.println("name ctime wtime");
-        for (int i = 0; i < p.length; i++) {
-            System.out.println(" " + p[i] + " " + comp[i]
-                    + " " + w[i]);
+        for (int i = 0; i < processes.size(); i++) {
+            System.out.println(" " + processes.get(i).getName() + " " + comp[i]+ " " + w[i]);
+           processes.get(i).setTurnAroundTime(comp[i]); // Turnaround time
+           processes.get(i).setResponseTime(startTimeMap.get(processes.get(i).getName()));
+           processes.get(i).setWaitingTime(w[i]);
+
+           turnAroundTime += processes.get(i).getTurnAroundTime();
+           responseTime += processes.get(i).getResponseTime();
+           waitingTime += processes.get(i).getWaitingTime();
 
             res = res + w[i];
             resc = resc + comp[i];
         }
 
-        System.out.println("Average waiting time is "
-                + (float) res / p.length);
-        System.out.println("Average compilation time is "
-                + (float) resc / p.length);
+        System.out.println("Average waiting time is " + (float) res / processes.size());
+        System.out.println("Average compilation time is "  + (float) resc / processes.size());
         System.out.println("Sequence is like that " + seq);
-    }
 
-    // Driver Code
-    public static void main(String args[]) {
-        // name of the process
-        String name[] = {"p1", "p2", "p3", "p4"};
+        return new Stat(turnAroundTime/ processes.size(),
+                responseTime / processes.size(),
+                waitingTime /processes.size());
 
-        // arrival for every process
-        int arrivaltime[] = {0, 1, 2, 3};
-
-        // burst time for every process
-        int bursttime[] = {10, 4, 5, 3};
-
-        // quantum time of each process
-        int q = 3;
-
-        // cal the function for output
-        roundRobin(name, arrivaltime, bursttime, q);
     }
 } 
